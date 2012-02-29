3a0ff67ba8ce738754741402194a80fe79aa760e
Matti Vesikivi <matti.vesikivi@iki.fi>
2012-02-29 21:39:34 +0200

    Lisätty ProdekoIlmoBundle, jossa tietomallintynkä

---
+++ src/Prodeko/IlmoBundle/Entity/Event.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Prodeko\IlmoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Prodeko\IlmoBundle\Entity\Event
+ */
+class Event
+{
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+    /**
+     * @var string $name
+     */
+    private $name;
+
+    /**
+     * @var text $description
+     */
+    private $description;
+
+
+    /**
+     * @var text $short_description
+     */
+    private $short_description;
+    
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set description
+     *
+     * @param text $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * Get description
+     *
+     * @return text 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+
+    /**
+     * Set short_description
+     *
+     * @param text $shortDescription
+     */
+    public function setShortDescription($shortDescription)
+    {
+        $this->short_description = $shortDescription;
+    }
+
+    /**
+     * Get short_description
+     *
+     * @return text 
+     */
+    public function getShortDescription()
+    {
+        return $this->short_description;
+    }
+}
---
+++ src/Prodeko/IlmoBundle/ProdekoIlmoBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Prodeko\IlmoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ProdekoIlmoBundle extends Bundle
+{
+}
