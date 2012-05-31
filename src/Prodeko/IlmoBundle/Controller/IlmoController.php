191299ed88ade968839a50824aa14edb8a6675f4
Kristian Sjolund <mediakeisari@prodeko.org>
2012-05-31 22:41:43 +0300

    Fix'd typo

--- src/Prodeko/IlmoBundle/Controller/IlmoController.php
+++ src/Prodeko/IlmoBundle/Controller/IlmoController.php
@@ -29,7 +29,7 @@ class IlmoController extends Controller
 			->getRepository('ProdekoIlmoBundle:Event')
 			->findOneBy(array('id' => $id));
 		$registrations = $this->getDoctrine()
-			->getRepository('ProdekoIlmomBundle:Registration')
+			->getRepository('ProdekoIlmoBundle:Registration')
 			->findBy(array('event_id' => $id));
 		$variables = array(
 				'eventname' => $event->getName(),
