dd0da3d2df78d2cb93d7b3fecdedc04bb0b069c0
Kristian Sjolund <mediakeisari@prodeko.org>
2012-05-31 19:05:24 +0300

    Lollotiloihloih

--- src/Prodeko/IlmoBundle/Controller/IlmoController.php
+++ src/Prodeko/IlmoBundle/Controller/IlmoController.php
@@ -14,9 +14,25 @@ class IlmoController extends Controller
 		return new Response("Homo");
 	}
 	
+	//Näyttää yhden tapahtuman tiedot
 	public function showAction($tapahtuma)
 	{
 		//TODO: implement "show event details"-controller
 		return $this->render('ProdekoIlmoBundle:Ilmo:event.html.twig', array('event' => $tapahtuma));
 	}
+	
+	// Näyttää lomakkeen jolla luodaan tapahtuma
+	public function createEventAction($tapahtuma) 
+	{
+		$form = $this->createFormBuilder($task)
+		->add('name', 'text')
+		->add('summary', 'textarea')
+		->add('description', 'textarea')
+		->getForm();
+		
+		return $this->render('ProdekoIlmoBundle:Ilmo:createEvent.html.twig', array(
+				'form' => $form->createView(),
+		));
+	}
+	
 }
