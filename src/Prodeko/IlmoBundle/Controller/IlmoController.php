1502eaf96dc971e27aa955090e7d4eea8bc3f0ab
Matti Vesikivi <matti.vesikivi@iki.fi>
2012-05-31 19:04:07 +0300

    tapahtumanluontikontrolleri implementoitu

--- src/Prodeko/IlmoBundle/Controller/IlmoController.php
+++ src/Prodeko/IlmoBundle/Controller/IlmoController.php
@@ -1,6 +1,10 @@
 <?php 
 namespace Prodeko\IlmoBundle\Controller;
 
+use Prodeko\IlmoBundle\Entity\Event;
+
+use Symfony\Component\HttpFoundation\Request;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
@@ -18,5 +22,22 @@ class IlmoController extends Controller
 	{
 		//TODO: implement "show event details"-controller
 		return $this->render('ProdekoIlmoBundle:Ilmo:event.html.twig', array('event' => $tapahtuma));
+	}
+	
+	public function createEventAction(Request $r)
+	{
+		$post_params = $r->request;
+		$name = $post_params->get("name");
+		$summary = $post_params->get("summary");
+		$description = $post_params->get("description");
+		$event = new Event();
+		
+		$event->setName($name);
+		$event->setSummary($summary);
+		$event->setDescription($description);
+		
+		$em = $this->getDoctrine()->getEntityManager();
+		$em->persist($event);
+		$em->flush();
 	}
 }
---
+++ src/Prodeko/IlmoBundle/Entity/FreeTextEntry.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Prodeko\IlmoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Prodeko\IlmoBundle\Entity\FreeTextEntry
+ */
+class FreeTextEntry
+{
+    /**
+     * @var integer $id
+     */
+    private $id;
+
+    /**
+     * @var text $content
+     */
+    private $content;
+
+    /**
+     * @var Prodeko\IlmoBundle\Entity\FreeTextField
+     */
+    private $field;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set content
+     *
+     * @param text $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Get content
+     *
+     * @return text 
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set field
+     *
+     * @param Prodeko\IlmoBundle\Entity\FreeTextField $field
+     */
+    public function setField(\Prodeko\IlmoBundle\Entity\FreeTextField $field)
+    {
+        $this->field = $field;
+    }
+
+    /**
+     * Get field
+     *
+     * @return Prodeko\IlmoBundle\Entity\FreeTextField 
+     */
+    public function getField()
+    {
+        return $this->field;
+    }
+    /**
+     * @var Prodeko\IlmoBundle\Entity\Registration
+     */
+    private $registration;
+
+
+    /**
+     * Set registration
+     *
+     * @param Prodeko\IlmoBundle\Entity\Registration $registration
+     */
+    public function setRegistration(\Prodeko\IlmoBundle\Entity\Registration $registration)
+    {
+        $this->registration = $registration;
+    }
+
+    /**
+     * Get registration
+     *
+     * @return Prodeko\IlmoBundle\Entity\Registration 
+     */
+    public function getRegistration()
+    {
+        return $this->registration;
+    }
+}
