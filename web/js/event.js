753671d47af000f56fefa91cffd1922c5b99e1f0
Valtter Vihervuori <valtter@vihervuori.fi>
2012-06-01 22:44:41 +0300

    Muotoiltu ilmolomake paremmin ja lisätty javascripti, joka näyttää lomakkeen sivua ladattaessa, jos siinä on ollut virheitä.

--- web/js/event.js
+++ web/js/event.js
@@ -1,3 +1,10 @@
 $(function() {
-	$("#subscriptionmodal").modal('hide');
+	//Tarkista, onko lomakkeeseen tullut virheitä edellisestä lähetyksestä
+	if ($('.formerror:parent').length == 0) {
+		//Jos virheitä ei ole tullut tai sivu ladataan ensimmäistä kertaa, tee lomakkeesta modaali, ja piilota se
+		$("#subscriptionmodal").modal('hide');
+	} else {
+		//Jos virheitä on tullut, tee lomakkeesta modaali ja näytä se.
+		$("#subscriptionmodal").modal('show');
+	}
 });
