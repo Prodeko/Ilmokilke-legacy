8ab9d81567915d33832c95393a6448d2b3cee3a0
Valtter Vihervuori <valtter@vihervuori.fi>
2012-06-10 04:06:08 +0300

    Täysin toimiva systeemi kenttien lisäämiseen ja poistoon silloin, kun tapahtumassa on jo ilmoittautuneita.
    Kristian voisi tarjota mulle kaljan tästä, ei meinaan ollu ihan helppo juttu. Tää tosin vaatii vielä testausta.

--- src/Prodeko/IlmoBundle/Controller/AdminController.php
+++ src/Prodeko/IlmoBundle/Controller/AdminController.php
@@ -2,6 +2,8 @@
 namespace Prodeko\IlmoBundle\Controller;
 
 
+use Prodeko\IlmoBundle\Entity\FreeTextEntry;
+
 use Prodeko\IlmoBundle\Entity\Quota;
 
 use Prodeko\IlmoBundle\Form\Type\RegistrationType;
@@ -29,9 +31,20 @@ class AdminController extends IlmoController
 		if ($id != 0) {
 			$event = $this->getDoctrine()
 			->getRepository('ProdekoIlmoBundle:Event')
-			->findOneBy(array('id' => $id));	
+			->findOneBy(array('id' => $id));
+			if ($event) {
+				$registrations = $event->getRegistrations();
+				if ($registrations && count($registrations) > 0) {
+					$state = 3; // Sellaisen tapahtuman muokkaus, jolla on ilmoittautumisia.
+				} else {
+					$state = 2; // Muokkaus, ei ilmoittautumisia.
+				}
+			} else {
+				echo "Ei löydy tapahtumaa!"; die; //TODO: Tähän ehkä joku exception handling
+			}
 		}
 		else {
+			$state = 1; // Uuden tapahtuman luonti.
 			$event = new Event();
 			$quota_names = array('I','II','III','IV','N');
 			
@@ -49,12 +62,75 @@ class AdminController extends IlmoController
 		//Tee ilmoittautumislomake, määrittely löytyy Prodeko\IlmoBundle\Form\Type\EventType
 		$form = $this->createForm(new EventType(), $event);
 		
-		if ($request->getMethod() == 'POST') {	
-			//Jos kyseessä on lähetetyn lomakkeen käsittely, anna lomakkeesta tulleet arvot eventille
+		//Jos kyseessä on lomakkeen käsittely, eikä lomakkeen näyttö
+		if ($request->getMethod() == 'POST') {
+			
+			if ($state == 3) { // Eli jos muokataan tapahtumaa, jossa on ilmoittautumisia
+				$originalFields = Array();
+				// Tallenna alkuperäiset kentät
+				foreach ($event->getFreeTextFields() as $field) $originalFields[] = $field;
+			}
+			
+			
+			//TODO: if ($form->isValid()) { ... } Jutut tähän väliin.
+			
+			//Anna lomakkeesta tulleet arvot eventille
 			$form->bindRequest($request);
 			$event = $form->getData();
-			//Tallenna tapahtuma
 			$em = $this->getDoctrine()->getEntityManager();
+			
+			if ($state == 3) { // Aloita kenttien lisäyksen ja poiston käsittely
+							   // Tämä on relevanttia vain jos muokataan tapahtumaa, jolla on ilmoittautumisia.
+				$newFields = Array();
+				$deletedFields = $originalFields;
+				foreach ($event->getFreeTextFields() as $key => $field) {
+					//Fieldillä on id <=> se on tallennettu tietokantaan <=> Se on pysynyt olemassaolevana.
+					if ($field->getId()) {
+						foreach ($deletedFields as $toDel) {
+							//Etsii sen kentän alkuperäisten listasta, jota uusi kenttä vastaa.
+							if ($toDel->getId() === $field->getId()) {
+								//Poista kenttä deletedFields-listasta.
+								unset($deletedFields[$key]);
+							}
+						}
+					} else {
+						//Jos fieldillä ei ole idiä, sen on oltava uusi.
+						$newFields[] = $field;
+					}
+				}
+				
+				// Lisää jokaiselle uudelle kentälle ilmoitukset kaikkiin olemassaoleviin ilmoittautumisiin, että kenttää ei ole täytetty.
+				foreach ($newFields as $newField) {
+					foreach ($registrations as $registration) {
+						// Luo entry, anna sille ominaisuudet ja tallenna se
+						$entry = new FreeTextEntry();
+						$entry->setContent('###ERROR###'); // Tai jotain...
+						$entry->setField($newField);
+						$entry->setRegistration($registration);
+						$em->persist($entry);
+					}
+				}
+				
+				// Poista kaikkien poistuneiden fieldien entryt (ei poista fieldejä, koska fieldien kierrätys)
+				foreach ($deletedFields as $toDel) {
+					$toDelId = $toDel->getId();
+					// Etsi esimmäiseltä ilmoittautuneelta se entry, joka vastaa poistettavaa kenttää
+					foreach ($registrations[0]->getFreeTextEntries() as $key => $entry) {
+						if ($entry->getField()->getId() === $toDelId) {
+							// Kun kenttä löytyy, tallenna sen avain ja lopeta etsiminen.
+							$delKey = $key;
+							break;
+						}	
+					}
+					// Poista jokaiselta ilmoittautuneelta kyseinen entry.
+					foreach ($registrations as $registration) {
+						$entries = $registration->getFreeTextEntries();
+						$em->remove($entries[$delKey]);
+					}
+				}
+			} // Lopeta kenttien lisäyksen ja poiston käsittely
+			
+			//Tallenna tapahtuma
 			$em->persist($event);
 			$em->flush();
 			//Ohjaa tarkastelemaan luotua tapahtumaa
@@ -75,7 +151,7 @@ class AdminController extends IlmoController
 		
 		$registrations = $event->getRegistrations();
 		$freeTextFields = $event->getFreeTextFields();
-		foreach ($registrations as $registration) {
+	/*	foreach ($registrations as $registration) {
 			foreach ($registration->getFreeTextEntries() as $entry) {
 				$field = $entry->getField();
 				$events = $field->getEvents(); //Voisiko tämän tehdä jollain querybuilderilla? Nyt jos on sata ilmoittautunutta, joista jokaisella on viisi entryä, joista jokaisella on field, jolla on vitusti tapahtumia, niin tässä menee joku miljoona vuotta iteroida.
@@ -85,7 +161,7 @@ class AdminController extends IlmoController
 					}
 				}
 			}
-		}
+		}*/
 		
 		return $this->render('ProdekoIlmoBundle:Ilmo:adminRegistrations.html.twig', array(
 				'event' => $event,
