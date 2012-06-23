b73e9a4c45eaa4c248cc023c36d2ab3a6d95f1fe
Valtter Vihervuori <valtter@vihervuori.fi>
2012-06-23 16:17:59 +0300

    Javascriptilla tehty tyylikkäämpi esitys radiobuttoneille ilmolomakkeessa.

--- web/js/event.js
+++ web/js/event.js
@@ -1,4 +1,33 @@
 $(function() {
+	function replaceRadioButtons($buttonDiv, $prototype) {
+		//Funktio piilottaa radiobuttonit annetussa divissä ja korvaa ne pseudo-radiobuttoneilla prototyypissä annetun mallin mukaan.
+		//Hae yksittäisen nappulan prototyyppi.
+		var $button = $prototype.find("button").clone();
+		
+		//Poista prototyyppi-divistä kaikki turha
+		$prototype.empty();
+		$prototype.removeAttr("style");
+		$prototype.removeAttr("id");
+		
+		//Lisää jokaista radiobuttonin labelia kohti nappula, jonka teksti on sama kuin labelilla
+		$buttonDiv.find("label").each(function() {
+			var $newButton = $button.clone().text($(this).text());
+			var $oldInput = $(this).prev("input:radio");
+			
+			//Anna buttoneille eventhandleri, joka tsekkaa oikean radiobuttonin.
+			$newButton.on("click", function() {
+				$oldInput.attr("checked", "checked");
+			});
+			
+			$prototype.append($newButton);
+		});
+		
+		//Piilota vanhat radiobuttonit
+		$buttonDiv.children().hide();
+		//Liitä uudet radiobuttonit
+		$buttonDiv.prepend($prototype);
+	}
+	
 	//Tarkista, onko lomakkeeseen tullut virheitä edellisestä lähetyksestä
 	//(Jos virheitä on tullut, .formerror - luokan diveillä on sisältöä, eli joku niistä on :parent)
 	if ($('.formerror:parent').length == 0) {
@@ -11,8 +40,17 @@ $(function() {
 		$('.formerror:parent').closest('fieldset').addClass('error');
 	}
 	
-	//Disabloi ilmoittautumisnappula lomakketta lähettäessä, estää tuplailmoittautumisen vahingossa.
+	//Etsi kaikki radiobuttoneja sisältävät divit ja korvaava prototyyppi
+	var $radioButtonDivs = $("div.controls:has(input:radio)");
+	var $prototype = $("#radiobuttons-prototype");
+	//Korvaa kaikki radiobuttonit tyylikkämillä napeilla
+	$radioButtonDivs.each(function() {
+		replaceRadioButtons($(this), $prototype.clone());
+	});
+	
+	
 	$('form').submit(function(){
+		//Disabloi ilmoittautumisnappula lomakketta lähettäessä, estää tuplailmoittautumisen vahingossa.
 	    $('input[type=submit]', this).attr('disabled', 'disabled');
 	});
 });
