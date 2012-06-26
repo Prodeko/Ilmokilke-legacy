4f58187ff85391c4eaf759843d3ba0d5e4a89f1e
Matti Vesikivi <matti.vesikivi@iki.fi>
2012-06-26 19:45:36 +0300

    en osaa

---
+++ web/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Nothing to see here..</title>
+  </head>
+  <body>
+    <h1>Ime paskaa</h1>
+    <img alt="Trollface" src="http://i0.kym-cdn.com/photos/images/original/000/096/044/trollface.jpg"/>
+  </body>
+</html>
