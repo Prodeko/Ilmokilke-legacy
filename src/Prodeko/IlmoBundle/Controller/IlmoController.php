4edf8fbd7c07d448ec465396c7a22bbdf7c618c7
Matti Vesikivi <matti.vesikivi@iki.fi>
2012-09-06 17:24:20 +0300

    Poistettu admin-sivun ylimääräinen footer ja vaihdettu vahvistussähköpostin url

--- src/Prodeko/IlmoBundle/Controller/IlmoController.php
+++ src/Prodeko/IlmoBundle/Controller/IlmoController.php
@@ -150,7 +150,7 @@ class IlmoController extends Controller
 	{
 		$messageBody = "Ilmoittautumisesi tapahtumaan " . $event->getName() . " on tallennettu.\n" .
 						"Voit poistaa ilmoittautumisesi allaolevasta linkistä.\n" . 
-						"http://ilmogilge.no-ip.org/app.php/fi/remove/" . $token;
+						"http://www.prodeko.fi/Ilmokilke/web/app.php/fi/remove/" . $token;
 		$message = \Swift_Message::newInstance()
 		->setSubject($event->getName() . ' / Ilmoittautuminen tallennettu')
 		->setFrom('ilmogilge@gmail.com')
